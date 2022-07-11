e5b2d269c48d551c2b2b218e1905cb0a47e020b9
Trevor Gross <tmgross@umich.edu>
2022-07-11 18:15:57 -0400

    Work on benchmarks

--- crates/stringmetrics/benches/levenshtein_bench.rs
+++ crates/stringmetrics/benches/levenshtein_bench.rs
@@ -2,8 +2,8 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use std::cmp::min;
 use stringmetrics::{levenshtein, levenshtein_limit, levenshtein_limit_weight, LevWeights};
 
-const STR_A: &str = "the fat cat";
-const STR_B: &str = "a mean bat";
+const STR_A: &str = "an orange cat";
+const STR_B: &str = "an auburn bat";
 
 const STR_A_LONG: &str = "Lorem ipsum dolor sit amet, consectetur adipiscing \
     elit. Nunc non dictum elit. Curabitur vitae dapibus dolor, id consequat \
@@ -38,9 +38,6 @@ pub fn bench_lev(c: &mut Criterion) {
     });
     c.bench_function("Base Levenshtein limit", |b| {
         b.iter(|| levenshtein_limit(black_box(STR_A), black_box(STR_B), black_box(40)))
-    });
-    c.bench_function("Quick Levenshtein limit", |b| {
-        b.iter(|| levenshtein_quick_limit(black_box(STR_A), black_box(STR_B), black_box(40)))
     });
     c.bench_function("Levenshtein Weights", |b| {
         b.iter(|| levenshtein_limit_weight(black_box(STR_A), black_box(STR_B), 100, &weights))
@@ -117,50 +114,6 @@ fn levenshtein_quick(a: &str, b: &str) -> u32 {
     }
 
     *v_prev.last().unwrap()
-}
-
-fn levenshtein_quick_limit(a: &str, b: &str, limit: u32) -> u32 {
-    let a_len = a.len() as u32;
-    let b_len = b.len() as u32;
-
-    if a_len == 0 {
-        return b_len;
-    }
-    if b_len == 0 {
-        return a_len;
-    }
-
-    let v_len = b_len + 1;
-    let mut v_prev: Vec<u32> = (0..(v_len)).collect();
-    let mut v_curr: Vec<u32> = vec![0; v_len as usize];
-    let mut current_max: u32 = 0;
-
-    for (i, a_item) in a.chars().enumerate() {
-        v_curr[0] = (i + 1) as u32;
-        // Fill out the rest of the row
-        for (j, b_item) in b.chars().enumerate() {
-            let ins_cost = v_curr[j] + 1;
-            let del_cost = v_prev[j + 1] + 1;
-            let sub_cost = match a_item == b_item {
-                true => v_prev[j],
-                false => v_prev[j] + 1,
-            };
-
-            v_curr[j + 1] = min(min(ins_cost, del_cost), sub_cost);
-        }
-
-        current_max = *v_curr.last().unwrap();
-
-        if current_max >= limit {
-            return limit;
-        }
-
-        // Move current row to previous for the next loop
-        // "Current" is always overwritten so we can just swap
-        std::mem::swap(&mut v_prev, &mut v_curr);
-    }
-
-    current_max
 }
 
 criterion_group!(
